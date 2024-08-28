64f74f26018670888eaaa753df23dd3b2629ed1b
Ruby <rubylee9311@gmail.com>
2024-08-28 14:06:56 +0800

    Initial Commit

---
+++ qa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qa
+//
+//  Created by user on 2024/8/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
